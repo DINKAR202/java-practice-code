8020394338347e33a09d60eb00733aa7c3567892
Dinkar Kumar <dinkarkumar.god@gmail.com>
2023-08-23 23:56:47 +0530

    updated

--- pattern problem/M/Dc.java
+++ pattern problem/M/Dc.java
@@ -1,9 +1,15 @@
 public class Dc {
     public static void main(String[] args){
-        for(int i = 1; i<=5; i++){
-            for(int j= 1; j<=5; j++){
-                if()
+        for(int r = 1; r<=5; r++){
+            for(int c= 1; c<=5; c++){
+                if(c==1 || (r==1 & c<=4) || ((r>=2 & r<=4) & c==5) || (r==5 & c<=4)){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
             }
+            System.out.println();
         }
     }
 }
